cf4dcc05329832ed1f0289e1a193132244d528d2
Sivashankar2122 <sivashankargg22@gmail.com>
2025-07-30 17:03:41 +0530

    2nd commit

--- lib/features/home/presantation/pages/home_screen.dart
+++ lib/features/home/presantation/pages/home_screen.dart
@@ -14,7 +14,7 @@ class HomeScreen extends StatelessWidget {
       body: Center(
         child: Text(
           AppStrings().welcomeMessage,
-          style: AppTextStyles().homeheading
+          style:  AppTextStyles().homeheading
         ),
       ),
     );
